601cde3786fd465bb777970b768eb6b96109e041
Neal Conner <neal@lamassu.is>
2018-01-10 12:21:18 -0500

    Update download.sh

--- logs/download.sh
+++ logs/download.sh
@@ -26,5 +26,5 @@ echo 'cash-out-unencrypted'
 echo 'bills-log-unencrypted'
 echo 'send-server-log'
 echo
-echo 'The first three encrypt the logs prior to sending them, thus you'll need to first import your PGP key on your server.'
+echo 'The first three encrypt the logs prior to sending them, thus you should first import your PGP key on your server.'
 echo
