5582ce8fad353b9f2aa0b3b8991052384e2736fa
Neal Conner <neal@lamassu.is>
2018-02-07 12:29:24 -0500

    Create ethereum.js

---
+++ switches/light/ethereum.js
@@ -0,0 +1,12 @@
+const coinUtils = require('../coin-utils')
+
+const common = require('./common')
+
+module.exports = {setup}
+
+function setup (dataDir) {
+  const coinRec = coinUtils.getCryptoCurrency('ETH')
+  common.firewall([coinRec.defaultPort])
+  const cmd = `/usr/local/bin/${coinRec.daemon} --datadir "${dataDir}" --syncmode="light" --cache 500 --rpc`
+  common.writeSupervisorConfig(coinRec, cmd)
+}
