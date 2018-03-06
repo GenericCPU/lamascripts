cac8647c17c2b762f769437da756dcac8e785dfb
Neal Conner <neal@lamassu.is>
2018-03-06 03:29:14 +0000

    Update btc.sh

--- updates/nodes/btc.sh
+++ updates/nodes/btc.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
+echo 'Updating Bitcoin Core. This may take a minute.'
 supervisorctl stop bitcoin
+echo 'Downloading...'
 curl -#o bitcoin.tar.gz https://bitcoin.org/bin/bitcoin-core-0.16.0/bitcoin-0.16.0-x86_64-linux-gnu.tar.gz
-tar -xvzf bitcoin.tar.gz
+tar -xvzf bitcoin.tar.gz  2>/dev/null
+echo 'Updating...'
 mv /usr/local/bin/bitcoind /usr/local/bin/bitcoind-old
 mv /usr/local/bin/bitcoin-cli /usr/local/bin/bitcoin-cli-old
 mv /usr/local/bin/bitcoin-tx /usr/local/bin/bitcoind-tx-old
