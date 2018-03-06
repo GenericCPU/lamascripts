bc26c62d0018bb4cc0bcf34ee91ade3108a6240a
Neal Conner <neal@lamassu.is>
2018-03-06 02:59:35 +0000

    Create btc.sh

---
+++ updates/nodes/btc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+supervisorctl stop bitcoin
+curl -#o bitcoin.tar.gz https://bitcoin.org/bin/bitcoin-core-0.16.0/bitcoin-0.16.0-x86_64-linux-gnu.tar.gz
+tar -xvzf bitcoin.tar.gz
+mv /usr/local/bin/bitcoind /usr/local/bin/bitcoind-old
+mv /usr/local/bin/bitcoin-cli /usr/local/bin/bitcoin-cli-old
+mv /usr/local/bin/bitcoin-tx /usr/local/bin/bitcoind-tx-old
+mv /usr/local/bin/bitcoin-qt /usr/local/bin/bitcoind-qt-old
+cp bitcoin-0.16.0/bin/* /usr/local/bin/
+supervisorctl start bitcoin
+echo 'Bitcoin Core is updated.'
