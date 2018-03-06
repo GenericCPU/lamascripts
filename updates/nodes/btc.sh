55e03caa09c98f1a91d6c937c948800bf2f229e5
Neal Conner <neal@lamassu.is>
2018-03-06 03:33:40 +0000

    Update btc.sh

--- updates/nodes/btc.sh
+++ updates/nodes/btc.sh
@@ -4,13 +4,15 @@ set -e
 echo 'Updating Bitcoin Core. This may take a minute.'
 supervisorctl stop bitcoin
 echo 'Downloading...'
-curl -#o bitcoin.tar.gz https://bitcoin.org/bin/bitcoin-core-0.16.0/bitcoin-0.16.0-x86_64-linux-gnu.tar.gz
-tar -xvzf bitcoin.tar.gz  2>/dev/null
+curl -#o /tmp/bitcoin.tar.gz https://bitcoin.org/bin/bitcoin-core-0.16.0/bitcoin-0.16.0-x86_64-linux-gnu.tar.gz
+tar -xvzf /tmp/bitcoin.tar.gz -C /tmp/ 2>/dev/null
 echo 'Updating...'
 mv /usr/local/bin/bitcoind /usr/local/bin/bitcoind-old
 mv /usr/local/bin/bitcoin-cli /usr/local/bin/bitcoin-cli-old
 mv /usr/local/bin/bitcoin-tx /usr/local/bin/bitcoind-tx-old
 mv /usr/local/bin/bitcoin-qt /usr/local/bin/bitcoind-qt-old
-cp bitcoin-0.16.0/bin/* /usr/local/bin/
+cp /tmp/bitcoin-0.16.0/bin/* /usr/local/bin/
+rm -r /tmp/bitcoin-0.16.0
+rm -r /tmp/bitcoin.tar.gz
 supervisorctl start bitcoin
 echo 'Bitcoin Core is updated.'
