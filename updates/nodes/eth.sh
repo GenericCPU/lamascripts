0c83add679ed33820669597c1e2aae8741981cfd
Neal Conner <neal@lamassu.is>
2018-03-07 17:52:29 -0500

    Update eth.sh

--- updates/nodes/eth.sh
+++ updates/nodes/eth.sh
@@ -4,12 +4,12 @@ set -e
 echo 'Updating Ethereum. This may take a minute.'
 supervisorctl stop ethereum
 echo 'Downloading...'
-curl -#o ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.8.1-1e67410e.tar.gz
+curl -#o ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.8.2-b8b9f7f4.tar.gz
 tar -xzf /tmp/ethereum.tar.gz -C /tmp/
 echo 'Updating...'
 mv /usr/local/bin/geth /usr/local/bin/geth-old
-cp geth-linux-amd64-1.8.1-1e67410e/geth /usr/local/bin/geth
-rm -r /tmp/geth-linux-amd64-1.8.1-1e67410e
+cp geth-linux-amd64-1.8.2-b8b9f7f4/geth /usr/local/bin/geth
+rm -r /tmp/geth-linux-amd64-1.8.2-b8b9f7f4
 rm /tmp/ethereum.tar.gz
 supervisorctl start ethereum
 echo 'Ethereum is updated.'
