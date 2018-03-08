8313d5479cc4c7f17fcdf247057e1cb3ec0f8473
Neal Conner <neal@lamassu.is>
2018-03-08 15:34:09 -0500

    Update eth.sh

--- updates/nodes/eth.sh
+++ updates/nodes/eth.sh
@@ -8,7 +8,7 @@ curl -#o /tmp/ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/get
 tar -xzf /tmp/ethereum.tar.gz -C /tmp/
 echo 'Updating...'
 mv /usr/local/bin/geth /usr/local/bin/geth-old
-cp geth-linux-amd64-1.8.2-b8b9f7f4/geth /usr/local/bin/geth
+cp /tmp/geth-linux-amd64-1.8.2-b8b9f7f4/geth /usr/local/bin/geth
 rm -r /tmp/geth-linux-amd64-1.8.2-b8b9f7f4
 rm /tmp/ethereum.tar.gz
 supervisorctl start ethereum
