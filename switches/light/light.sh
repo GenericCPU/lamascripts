93ece80122275db548369c04ab826e1c9cbafce0
Neal Conner <neal@lamassu.is>
2018-03-21 13:41:11 -0400

    Update light.sh

--- switches/light/light.sh
+++ switches/light/light.sh
@@ -1,9 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -e
 
+echo 'Setting Ethereum to light mode. This may take up to 10 minutes.'
 supervisorctl stop ethereum
 curl -#o /usr/lib/node_modules/lamassu-server/lib/blockchain/ethereum.js https://raw.githubusercontent.com/naconner/lamascripts/master/switches/light/ethereum.js
 curl -#o /etc/supervisor/conf.d/ethereum.conf https://raw.githubusercontent.com/naconner/lamascripts/master/switches/light/ethereum.conf
 cd /mnt/blockchains/ethereum/geth/
-rm chaindata/*
+echo 'Deleting old chaindata.'
+find chaindata/* -type f -delete
+echo 'Complete. Rebooting the server. You may log back in after a minute.'
 shutdown -r now
