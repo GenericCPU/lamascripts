14bb37dd161964166810d71fc57a68b766889cb8
Neal Conner <neal@lamassu.is>
2018-04-14 13:47:02 -0400

    Create zec.sh

---
+++ updates/nodes/zec.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo 'Updating Zcash. This may take a minute.'
+supervisorctl stop zcash
+echo 'Downloading...'
+curl -#o /tmp/zcash.tar.gz https://z.cash/downloads/zcash-1.1.0-linux64.tar.gz
+tar -xzf /tmp/zcash.tar.gz -C /tmp/
+echo 'Updating...'
+mv /usr/local/bin/zcashd /usr/local/bin/zcashd-old
+mv /usr/local/bin/zcash-cli /usr/local/bin/zcash-cli-old
+mv /usr/local/bin/zcash-tx /usr/local/bin/zcash-tx-old
+mv /usr/local/bin/zcash-fetch-params /usr/local/bin/zcash-fetch-params-old
+cp /tmp/zcash-1.1.0/bin/ /usr/local/bin/
+rm -r /tmp/zcash-1.1.0
+rm /tmp/zcash.tar.gz
+supervisorctl start zcash
+echo 'Zcash is updated.'
