2351f7ee31308ac2678b64b86fef637518f6ad71
Neal Conner <neal@lamassu.is>
2018-04-14 14:01:38 -0400

    Update zec.sh

--- updates/nodes/zec.sh
+++ updates/nodes/zec.sh
@@ -11,7 +11,7 @@ mv /usr/local/bin/zcashd /usr/local/bin/zcashd-old
 mv /usr/local/bin/zcash-cli /usr/local/bin/zcash-cli-old
 mv /usr/local/bin/zcash-tx /usr/local/bin/zcash-tx-old
 mv /usr/local/bin/zcash-fetch-params /usr/local/bin/zcash-fetch-params-old
-cp /tmp/zcash-1.1.0/bin/ /usr/local/bin/
+cp /tmp/zcash-1.1.0/bin/* /usr/local/bin/
 rm -r /tmp/zcash-1.1.0
 rm /tmp/zcash.tar.gz
 supervisorctl start zcash
