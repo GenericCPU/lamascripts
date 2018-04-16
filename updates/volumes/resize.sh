86164b99f3a9dff5c4fbe74597e01af031e2a8fd
Neal Conner <neal@lamassu.is>
2018-04-16 16:56:33 -0400

    Update resize.sh

--- updates/volumes/resize.sh
+++ updates/volumes/resize.sh
@@ -5,7 +5,7 @@ VOLUME=$(ls /dev/disk/by-id/scsi-0DO_Volume_* | grep -m1 -oP '(?<=by-id/).+$')
 supervisorctl stop all
 sleep 3s
 resize2fs /dev/disk/by-id/$VOLUME
-sleep 3s
+sleep 5s
 supervisorctl start all
 echo
 echo "Done! Volume resized and services restarted."
