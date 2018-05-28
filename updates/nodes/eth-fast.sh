b477de9af8d0d436d6b71f53958abb4ffcc7c0eb
Neal Conner <neal@lamassu.is>
2018-05-28 19:06:43 -0400

    Create eth-fast.sh

---
+++ updates/nodes/eth-fast.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo 'Updating Ethereum and setting to fast mode. This may take a minute.'
+supervisorctl stop ethereum
+echo 'Downloading...'
+curl -#o /tmp/ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.8.9-ff9b1461.tar.gz
+tar -xzf /tmp/ethereum.tar.gz -C /tmp/
+echo 'Updating...'
+mv /usr/local/bin/geth /usr/local/bin/geth-old
+cp /tmp/geth-linux-amd64-1.8.9-ff9b1461/geth /usr/local/bin/geth
+rm -r /tmp/geth-linux-amd64-1.8.9-ff9b1461
+rm /tmp/ethereum.tar.gz
+curl -#o /usr/lib/node_modules/lamassu-server/lib/blockchain/ethereum.js https://raw.githubusercontent.com/lamassu/lamassu-server/v5/lib/blockchain/ethereum.js
+curl -#o /etc/supervisor/conf.d/ethereum.conf https://raw.githubusercontent.com/naconner/lamascripts/master/switches/fast/ethereum.conf
+echo 'Complete. Rebooting the server. You may log back in after a minute.'
+shutdown -r now
