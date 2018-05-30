2e60fc990860f7012d5b8ab1ad82ef8238a563c5
Neal Conner <neal@lamassu.is>
2018-05-30 18:37:14 -0400

    Update eth-fast.sh

--- updates/nodes/eth-fast.sh
+++ updates/nodes/eth-fast.sh
@@ -4,12 +4,12 @@ set -e
 echo 'Updating Ethereum and setting to fast mode. This may take a minute.'
 supervisorctl stop ethereum
 echo 'Downloading...'
-curl -#o /tmp/ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.8.9-ff9b1461.tar.gz
+curl -#o /tmp/ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.8.10-eae63c51.tar.gz
 tar -xzf /tmp/ethereum.tar.gz -C /tmp/
 echo 'Updating...'
 mv /usr/local/bin/geth /usr/local/bin/geth-old
-cp /tmp/geth-linux-amd64-1.8.9-ff9b1461/geth /usr/local/bin/geth
-rm -r /tmp/geth-linux-amd64-1.8.9-ff9b1461
+cp /tmp/geth-linux-amd64-1.8.10-eae63c51/geth /usr/local/bin/geth
+rm -r /tmp/geth-linux-amd64-1.8.10-eae63c51
 rm /tmp/ethereum.tar.gz
 curl -#o /usr/lib/node_modules/lamassu-server/lib/blockchain/ethereum.js https://raw.githubusercontent.com/lamassu/lamassu-server/v5/lib/blockchain/ethereum.js
 curl -#o /etc/supervisor/conf.d/ethereum.conf https://raw.githubusercontent.com/naconner/lamascripts/master/switches/fast/ethereum.conf
