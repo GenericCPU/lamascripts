f8a52964c6a1a1359f1dca15f7e185b28e7552de
Neal Conner <neal@lamassu.is>
2018-05-31 10:54:25 -0400

    Create ltc.sh

---
+++ updates/nodes/ltc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo 'Updating Litecoin Core. This may take a minute.'
+supervisorctl stop litecoin
+echo 'Downloading...'
+curl -#o /tmp/litecoin.tar.gz https://download.litecoin.org/litecoin-0.16.0/linux/litecoin-0.16.0-x86_64-linux-gnu.tar.gz
+tar -xzf /tmp/litecoin.tar.gz -C /tmp/
+echo 'Updating...'
+mv /usr/local/bin/litecoind /usr/local/bin/litecoind-old
+mv /usr/local/bin/litecoin-cli /usr/local/bin/litecoin-cli-old
+mv /usr/local/bin/litecoin-tx /usr/local/bin/litecoin-tx-old
+mv /usr/local/bin/litecoin-qt /usr/local/bin/litecoin-qt-old
+cp /tmp/litecoin-0.16.0/bin/* /usr/local/bin/
+rm -r /tmp/litecoin-0.16.0
+rm /tmp/litecoin.tar.gz
+supervisorctl start litecoin
+echo 'Litecoin Core is updated.'
