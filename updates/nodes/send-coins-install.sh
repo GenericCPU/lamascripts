d5e7ef8184d54d16558964e425d227f62ec11b3a
Neal Conner <neal@lamassu.is>
2018-06-30 12:33:36 +0100

    Update send-coins-install.sh

--- updates/nodes/send-coins-install.sh
+++ updates/nodes/send-coins-install.sh
@@ -3,16 +3,16 @@ set -e
 
 cd /usr/lib/node_modules/lamassu-server/
 echo 'Installing new dependencies...'
-npm install ethereumjs-util@5.2.0 2>/dev/null
-npm install inquirer@5.2.0 2>/dev/null
+npm install ethereumjs-util@5.2.0 &>/dev/null
+npm install inquirer@5.2.0 &>/dev/null
 echo 'Pulling down changes...'
-curl -#o lib/plugins/wallet/geth/geth.js https://raw.githubusercontent.com/lamassu/lamassu-server/d14256702eb080592d1f414c0a98a980cee3fb89/lib/plugins/wallet/geth/geth.js 2>/dev/null
-curl -#o lib/wallet.js https://raw.githubusercontent.com/lamassu/lamassu-server/d14256702eb080592d1f414c0a98a980cee3fb89/lib/wallet.js 2>/dev/null
-curl -#o bin/lamassu-send-coins https://raw.githubusercontent.com/lamassu/lamassu-server/d14256702eb080592d1f414c0a98a980cee3fb89/bin/lamassu-send-coins 2>/dev/null
+curl -#o lib/plugins/wallet/geth/geth.js https://raw.githubusercontent.com/lamassu/lamassu-server/d14256702eb080592d1f414c0a98a980cee3fb89/lib/plugins/wallet/geth/geth.js &>/dev/null
+curl -#o lib/wallet.js https://raw.githubusercontent.com/lamassu/lamassu-server/d14256702eb080592d1f414c0a98a980cee3fb89/lib/wallet.js &>/dev/null
+curl -#o bin/lamassu-send-coins https://raw.githubusercontent.com/lamassu/lamassu-server/d14256702eb080592d1f414c0a98a980cee3fb89/bin/lamassu-send-coins &>/dev/null
 chmod 755 bin/lamassu-send-coins
 ln -s /usr/lib/node_modules/lamassu-server/bin/lamassu-send-coins /usr/bin/lamassu-send-coins
 echo 'Restarting lamassu-server...'
-supervisorctl restart lamassu-server 2>/dev/null
+supervisorctl restart lamassu-server &>/dev/null
 echo 'Done.'
 echo
 echo 'Use may use lamassu-send-coins to manually create transactions.'
