9ebc87d44d3a962958f9854f1c621964cc2ae1b6
Neal Conner <neal@lamassu.is>
2018-06-30 23:58:25 +0100

    Update litecoin.sh

--- wallet-repair/litecoin.sh
+++ wallet-repair/litecoin.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 set -e
 
+d=$(date -u "+%Y%m%d")
+
 echo
 echo 'Stopping Litecoin...'
-supervisorctl stop litecoin
+supervisorctl stop litecoin &>/dev/null
 
 echo 'Clearing the database, chainstate, and blocks...'
 cd /mnt/blockchains/litecoin
-mkdir empty
-rsync -a --delete empty/ database/
-rsync -a --delete empty/ chainstate/
-rsync -a --delete empty/ blocks/
+mkdir empty-$d
+rsync -a --delete empty-$d/ database/
+rsync -a --delete empty-$d/ chainstate/
+rsync -a --delete empty-$d/ blocks/
 
 echo 'Updating Litecoin...'
-curl -#o /tmp/litecoin.tar.gz https://download.litecoin.org/litecoin-0.16.0/linux/litecoin-0.16.0-x86_64-linux-gnu.tar.gz
+curl -#o /tmp/litecoin.tar.gz https://download.litecoin.org/litecoin-0.16.0/linux/litecoin-0.16.0-x86_64-linux-gnu.tar.gz  &>/dev/null
 tar -xzf /tmp/litecoin.tar.gz -C /tmp/
 mv /usr/local/bin/litecoind /usr/local/bin/litecoind-old
 mv /usr/local/bin/litecoin-cli /usr/local/bin/litecoin-cli-old
@@ -24,11 +26,11 @@ rm -r /tmp/litecoin-0.16.0
 rm /tmp/litecoin.tar.gz
 
 echo "Resetting Litecoin's supervisor configuration..."
-curl -#o /etc/supervisor/conf.d/litecoin.conf https://raw.githubusercontent.com/lamassu/lamassu-server/staging/lib/blockchain/litecoin.js
-supervisorctl reread
+curl -#o /etc/supervisor/conf.d/litecoin.conf https://raw.githubusercontent.com/naconner/lamascripts/master/switches/reindex/litecoin.conf-orig  &>/dev/null
+supervisorctl reread &>/dev/null
 
 echo 'Starting Litecoin...'
-supervisorctl start litecoin
+supervisorctl start litecoin &>/dev/null
 
 echo 'Done!'
 echo
