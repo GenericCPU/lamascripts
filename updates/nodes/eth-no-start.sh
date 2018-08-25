f985d9b0147e782724dee402d1bfcfc9419f95da
Neal Conner <neal@lamassu.is>
2018-08-24 20:37:57 -0400

    Update eth-no-start.sh

--- updates/nodes/eth-no-start.sh
+++ updates/nodes/eth-no-start.sh
@@ -4,11 +4,11 @@ set -e
 echo 'Updating Ethereum. This may take a minute.'
 supervisorctl stop ethereum
 echo 'Downloading...'
-curl -#o /tmp/ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.8.13-225171a4.tar.gz
+curl -#o /tmp/ethereum.tar.gz https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.8.14-316fc7ec.tar.gz
 tar -xzf /tmp/ethereum.tar.gz -C /tmp/
 echo 'Updating...'
 mv /usr/local/bin/geth /usr/local/bin/geth-old
-cp /tmp/geth-linux-amd64-1.8.13-225171a4/geth /usr/local/bin/geth
-rm -r /tmp/geth-linux-amd64-1.8.13-225171a4
+cp /tmp/geth-linux-amd64-1.8.14-316fc7ec/geth /usr/local/bin/geth
+rm -r /tmp/geth-linux-amd64-1.8.14-316fc7ec
 rm /tmp/ethereum.tar.gz
 echo 'Ethereum is updated.'
