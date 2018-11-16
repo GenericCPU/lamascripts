58ff9450da875c5b18e887d4c50d20520f3e9b7f
Neal Conner <neal@lamassu.is>
2018-11-16 14:31:14 -0500

    Update bch.sh

--- updates/nodes/bch.sh
+++ updates/nodes/bch.sh
@@ -4,14 +4,14 @@ set -e
 echo 'Updating Bitcoin Cash. This may take a minute.'
 supervisorctl stop bitcoincash
 echo 'Downloading...'
-curl -#o /tmp/bitcoincash.tar.gz https://download.bitcoinabc.org/0.18.2/linux/bitcoin-abc-0.18.2-x86_64-linux-gnu.tar.gz
+curl -#o /tmp/bitcoincash.tar.gz https://download.bitcoinabc.org/0.18.4/linux/bitcoin-abc-0.18.4-x86_64-linux-gnu.tar.gz
 tar -xzf /tmp/bitcoincash.tar.gz -C /tmp/
 echo 'Updating...'
 mv /usr/local/bin/bitcoincashd /usr/local/bin/bitcoincashd-old
 mv /usr/local/bin/bitcoincash-cli /usr/local/bin/bitcoin-cli-old
-cp /tmp/bitcoin-abc-0.18.2/bin/bitcoind /usr/local/bin/bitcoincashd
-cp /tmp/bitcoin-abc-0.18.2/bin/bitcoin-cli /usr/local/bin/bitcoincash-cli
-rm -r /tmp/bitcoin-abc-0.18.2
+cp /tmp/bitcoin-abc-0.18.4/bin/bitcoind /usr/local/bin/bitcoincashd
+cp /tmp/bitcoin-abc-0.18.4/bin/bitcoin-cli /usr/local/bin/bitcoincash-cli
+rm -r /tmp/bitcoin-abc-0.18.4
 rm /tmp/bitcoincash.tar.gz
 supervisorctl start bitcoincash
 echo 'Bitcoin Cash is updated.'
