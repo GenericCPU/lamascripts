65f1998687c96e5b39287cb42b090587e3037dca
Neal Conner <neal@lamassu.is>
2018-11-21 21:18:07 +0000

    Create coin-scaling-lib.sh

---
+++ patch/coin-scaling-lib.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+supervisorctl stop lamassu-server
+curl -#o /usr/lib/node_modules/lamassu-server/lib/coin-utils.js https://raw.githubusercontent.com/lamassu/lamassu-server/release/lib/coin-utils.js
+curl -#o /usr/lib/node_modules/lamassu-server/lib/plugins/common/kraken.js https://raw.githubusercontent.com/lamassu/lamassu-server/release/lib/plugins/common/kraken.js
+curl -#o /usr/lib/node_modules/lamassu-server/lib/plugins/exchange/bitstamp/bitstamp.js https://raw.githubusercontent.com/lamassu/lamassu-server/release/lib/plugins/exchange/bitstamp/bitstamp.js
+curl -#o /usr/lib/node_modules/lamassu-server/lib/plugins/exchange/kraken/kraken.js https://raw.githubusercontent.com/lamassu/lamassu-server/release/lib/plugins/exchange/kraken/kraken.js
+supervisorctl start lamassu-server
