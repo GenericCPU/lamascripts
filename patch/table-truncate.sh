a384db9a928d2724a1069038735bf409f7b12d5e
Neal Conner <neal@lamassu.is>
2018-12-21 00:27:17 -0500

    Update table-truncate.sh

--- patch/table-truncate.sh
+++ patch/table-truncate.sh
@@ -2,7 +2,13 @@
 
 supervisorctl stop lamassu-server lamassu-admin-server
 su - postgres -c "pg_dump lamassu > /tmp/lamassu-database.bak"
+echo 'Truncating machine_pings...'
 su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM machine_pings WHERE created < NOW() - INTERVAL '1 hour'\""
+echo 'Truncating machine_events...'
 su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM machine_events WHERE created < NOW() - INTERVAL '1 hour'\""
+echo 'Truncating server_events...'
 su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM server_events WHERE created < NOW() - INTERVAL '1 hour'\""
+echo 'Truncating machine logs...'
+su - postgres -c "psql \"lamassu\" -Atc \"DELETE FROM logs WHERE timestamp < NOW() - INTERVAL '1 day'\""
+echo 'Done.'
 supervisorctl start lamassu-server lamassu-admin-server
