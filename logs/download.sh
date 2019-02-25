007d92169337a0accd8b2b32b3219e324ced603f
Neal Conner <neal@lamassu.is>
2019-02-25 13:28:25 -0500

    Update download.sh

--- logs/download.sh
+++ logs/download.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
+bold=$(tput bold)
+normal=$(tput sgr0)
 
 curl -#o /usr/local/bin/cash-in-log https://raw.githubusercontent.com/naconner/lamascripts/master/logs/cash-in-log 2>/dev/null
 curl -#o /usr/local/bin/cash-out-log https://raw.githubusercontent.com/naconner/lamascripts/master/logs/cash-out-log 2>/dev/null
@@ -16,6 +18,8 @@ curl -#o /usr/local/bin/out-actions-unencrypted https://raw.githubusercontent.co
 curl -#o /usr/local/bin/export-out-actions https://raw.githubusercontent.com/naconner/lamascripts/master/logs/export-out-actions 2>/dev/null
 curl -#o /usr/local/bin/customers-log https://raw.githubusercontent.com/naconner/lamascripts/master/logs/customers-log 2>/dev/null
 curl -#o /usr/local/bin/export-customers https://raw.githubusercontent.com/naconner/lamascripts/master/logs/export-customers 2>/dev/null
+curl -#o /usr/local/bin/sanctions-log https://raw.githubusercontent.com/naconner/lamascripts/master/logs/sanctions-log 2>/dev/null
+curl -#o /usr/local/bin/export-sanctions-log https://raw.githubusercontent.com/naconner/lamascripts/master/logs/export-sanctions-log 2>/dev/null
 chmod 755 /usr/local/bin/cash-in-log
 chmod 755 /usr/local/bin/cash-out-log
 chmod 755 /usr/local/bin/bills-log
@@ -31,17 +35,37 @@ chmod 755 /usr/local/bin/out-actions-unencrypted
 chmod 755 /usr/local/bin/export-out-actions
 chmod 755 /usr/local/bin/customers-log
 chmod 755 /usr/local/bin/export-customers
+chmod 755 /usr/local/bin/sanctions-log
+chmod 755 /usr/local/bin/export-sanctions-log
 echo
 echo 'Done! You may now use the following commands, followed by an email address:'
 echo
-echo 'cash-in-log'
-echo 'cash-out-log'
-echo 'bills-log'
-echo 'customers-log'
-echo 'cash-in-unencrypted'
-echo 'cash-out-unencrypted'
-echo 'bills-log-unencrypted'
-echo 'send-server-log'
+echo "${bold}Encrypted:${normal}" 
 echo
-echo 'The first four encrypt the logs prior to sending them, thus should first import a PGP key onto your server. Otherwise, use the unencrypted commands.'
+echo "cash-in-log"
+echo "bills-log"
+echo "cash-out-log"
+echo "cash-out-actions"
+echo "customers-log"
+echo "sanctions-log"
+echo
+echo "${bold}Unencrypted:${normal}"
+echo
+echo "cash-in-unencrypted"
+echo "bills-log-unencrypted"
+echo "cash-out-unencrypted"
+echo "out-actions-unencrypted"
+echo "send-server-log"
+echo
+echo "${bold}Exports for SCP downloads:${normal}"
+echo
+echo "export-cash-in"
+echo "export-bills"
+echo "export-cash-out"
+echo "export-out-actions"
+echo "export-customers"
+echo "export-sanctions-log"
+echo "export-machine-logs"
+echo
+echo 'Refer to the knowledgebase for usage guidelines.'
 echo
