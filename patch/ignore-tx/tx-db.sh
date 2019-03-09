9aab92e900287450497765c332408ee026002ee4
Neal Conner <neal@lamassu.is>
2019-03-09 00:24:42 +0000

    Create tx-db.sh

---
+++ patch/ignore-tx/tx-db.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+echo
+echo "Patching server..."
+echo
+supervisorctl stop lamassu-server
+curl -#o $(npm root -g)/lamassu-server/lib/error.js https://raw.githubusercontent.com/naconner/lamascripts/master/patch/ignore-tx/error.js
+curl -#o $(npm root -g)/lamassu-server/lib/routes.js https://raw.githubusercontent.com/naconner/lamascripts/master/patch/ignore-tx/routes.js
+curl -#o $(npm root -g)/lamassu-server/lib/tx.js https://raw.githubusercontent.com/naconner/lamascripts/master/patch/ignore-tx/tx.js
+supervisorctl start lamassu-server
+echo
+echo "Patch applied."
+echo
