49d577e19efaac46a59f422cced45aa33947665f
Neal Conner <neal@lamassu.is>
2019-04-01 18:30:18 -0400

    Update update.sh

--- patch/coinatmradar/update.sh
+++ patch/coinatmradar/update.sh
@@ -7,7 +7,7 @@ echo
 echo "Applying patch for Coin ATM Radar support..."
 supervisorctl stop lamassu-server lamassu-admin-server &> /dev/null
 curl -#o $(npm root -g)/lamassu-server/lib/plugins.js https://raw.githubusercontent.com/lamassu/lamassu-server/206cb902af4a2fd252aedcf5ec2cf4cffab4ee6a/lib/plugins.js &> /dev/null
-curl -#o $(npm root -g)/lamassu-server/bin/lamassu-coinatmradar https://raw.githubusercontent.com/lamassu/lamassu-server/d9befeedf55026d1aec9673358831dfe4af78bbd/bin/lamassu-coinatmradar &> /dev/null
+curl -#o $(npm root -g)/lamassu-server/bin/lamassu-coinatmradar https://raw.githubusercontent.com/lamassu/lamassu-server/f1e4f3aade297eb51edf4075aec4403b430dd968/bin/lamassu-coinatmradar &> /dev/null
 export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 supervisorctl start lamassu-server lamassu-admin-server &> /dev/null
 echo
