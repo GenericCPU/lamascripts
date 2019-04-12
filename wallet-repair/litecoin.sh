25bd06c17f65c5c752ca08862b698351d43e7448
Neal Conner <neal@lamassu.is>
2019-04-12 20:40:35 +0100

    Update litecoin.sh

--- wallet-repair/litecoin.sh
+++ wallet-repair/litecoin.sh
@@ -15,14 +15,14 @@ rsync -a --delete empty-$d/ chainstate/
 rsync -a --delete empty-$d/ blocks/
 
 echo 'Updating Litecoin...'
-curl -#o /tmp/litecoin.tar.gz https://download.litecoin.org/litecoin-0.16.0/linux/litecoin-0.16.0-x86_64-linux-gnu.tar.gz  &>/dev/null
+curl -#o /tmp/litecoin.tar.gz https://download.litecoin.org/litecoin-0.16.3/linux/litecoin-0.16.3-x86_64-linux-gnu.tar.gz &>/dev/null
 tar -xzf /tmp/litecoin.tar.gz -C /tmp/
 mv /usr/local/bin/litecoind /usr/local/bin/litecoind-old
 mv /usr/local/bin/litecoin-cli /usr/local/bin/litecoin-cli-old
 mv /usr/local/bin/litecoin-tx /usr/local/bin/litecoin-tx-old
 mv /usr/local/bin/litecoin-qt /usr/local/bin/litecoin-qt-old
-cp /tmp/litecoin-0.16.0/bin/* /usr/local/bin/
-rm -r /tmp/litecoin-0.16.0
+cp /tmp/litecoin-0.16.3/bin/* /usr/local/bin/
+rm -r /tmp/litecoin-0.16.3
 rm /tmp/litecoin.tar.gz
 
 echo 'Clearing Litecoin logs...'
