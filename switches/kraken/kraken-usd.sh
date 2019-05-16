18a0f64e8ea547ca95b05979284ddbee47eed38d
Brandon Cryderman <GenericCPU@users.noreply.github.com>
2019-05-16 16:49:34 -0400

    Update kraken-usd.sh

--- switches/kraken/kraken-usd.sh
+++ switches/kraken/kraken-usd.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Finding kraken.js..."
-cd $(dirname $(find / -name kraken.js |  grep "/lamassu-server/" | grep exchange))
+cd $(dirname $(find / -name kraken.js |   grep "/lamassu-server/" | grep exchange))
 echo "Replacing with USD-default Kraken trader..."
 curl -#o kraken.js https://raw.githubusercontent.com/lamassusupport/lamascripts/master/switches/kraken/kraken-usd.js
 echo "Restarting lamassu-server..."
