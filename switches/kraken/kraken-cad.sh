c4f064bcd0a97cbbb1d59bbab6f107519d1678ff
Brandon Cryderman <GenericCPU@users.noreply.github.com>
2019-05-17 10:09:48 -0400

    Update kraken-cad.sh

--- switches/kraken/kraken-cad.sh
+++ switches/kraken/kraken-cad.sh
@@ -3,14 +3,12 @@ set -e
 
 echo "Finding exchange kraken.js file..."
 
-cd $(dirname $(find / -name kraken.js |  grep "/lamassu-server/" | grep exchange))
 echo "Replacing with CAD-default Kraken trader..."
-curl -#o kraken.js https://raw.githubusercontent.com/lamassusupport/lamascripts/master/switches/kraken/kraken-cad.js
+curl -#o $(npm root -g)/lamassu-server/lib/plugins/exchange/kraken/kraken.js https://raw.githubusercontent.com/lamassusupport/lamascripts/master/switches/kraken/kraken-cad.js
 
 echo "Finding common kraken.js file..."
 echo "Adding CAD pairs to Kraken..."
-cd $(dirname $(find / -name kraken.js | grep "/lamassu-server/" | grep common))
-curl -#o kraken.js https://raw.githubusercontent.com/lamassusupport/lamascripts/master/switches/kraken/kraken-common-cad.js
+curl -#o $(npm root -g)/lamassu-server/lib/plugins/common/kraken.js https://raw.githubusercontent.com/lamassusupport/lamascripts/master/switches/kraken/kraken-common-cad.js
 
 echo "Restarting lamassu-server..."
 supervisorctl restart lamassu-server
