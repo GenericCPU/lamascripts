e6a4cb88e078cbd05d249a5b8d338b02486bc8b3
Brandon Cryderman <GenericCPU@users.noreply.github.com>
2019-05-17 10:07:03 -0400

    Update bitstamp-usd.sh

--- switches/bitstamp/bitstamp-usd.sh
+++ switches/bitstamp/bitstamp-usd.sh
@@ -2,9 +2,8 @@
 set -e
 
 echo "Finding bitstamp.js..."
-cd $(dirname $(find / -name bitstamp.js |  grep "/lamassu-server/" | grep exchange))
 echo "Replacing with USD-default Bitstamp trader..."
-curl -#o bitstamp.js https://raw.githubusercontent.com/lamassusupport/lamascripts/master/switches/bitstamp/bitstamp-usd.js
+curl -#o $(npm root -g)/lamassu-server/lib/plugins/exchange/bitstamp/bitstamp.js https://raw.githubusercontent.com/lamassusupport/lamascripts/master/switches/bitstamp/bitstamp-usd.js
 echo "Restarting lamassu-server..."
 supervisorctl restart lamassu-server
 echo "Change complete."
