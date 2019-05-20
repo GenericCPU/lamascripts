3d5274c112b6d3bf4b0dd1d0ee3712b5546326c7
Lamassu Support <support@lamassu.is>
2019-05-20 12:39:17 -0400

    Update logger-ticker-patch.sh

--- patch/logger/logger-ticker-patch.sh
+++ patch/logger/logger-ticker-patch.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+supervisorctl stop lamassu-server lamassu-admin-server
 curl -#o $(npm root -g)/lamassu-server/lib/ticker.js https://raw.githubusercontent.com/LamassuSupport/lamascripts/master/patch/logger/ticker.js
 curl -#o $(npm root -g)/lamassu-server/lib/logger.js https://raw.githubusercontent.com/LamassuSupport/lamascripts/master/patch/logger/logger.js
 echo "Patched."
+supervisorctl start lamassu-server lamassu-admin-server
