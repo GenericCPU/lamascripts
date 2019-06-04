aabdb23aebb02a86efea275b2de5c97446d482ef
Lamassu Support <support@lamassu.is>
2019-06-04 17:00:55 -0400

    Update patch.sh

--- patch/ticker-bignumber/patch.sh
+++ patch/ticker-bignumber/patch.sh
@@ -6,9 +6,9 @@ echo
 echo "Patching the tickers..."
 echo
 cd $(npm root -g)/lamassu-server/lib/plugins/ticker/
-curl -#o bitpay.js https://raw.githubusercontent.com/LamassuSupport/lamascripts/master/patch/ticker-bignumber/bitpay.js
-curl -#o bitstamp.js https://raw.githubusercontent.com/LamassuSupport/lamascripts/master/patch/ticker-bignumber/bitstamp.js
-curl -#o kraken.js https://raw.githubusercontent.com/LamassuSupport/lamascripts/master/patch/ticker-bignumber/kraken.js
+curl -#o bitpay/bitpay.js https://raw.githubusercontent.com/LamassuSupport/lamascripts/master/patch/ticker-bignumber/bitpay.js
+curl -#o bitstamp/bitstamp.js https://raw.githubusercontent.com/LamassuSupport/lamascripts/master/patch/ticker-bignumber/bitstamp.js
+curl -#o kraken/kraken.js https://raw.githubusercontent.com/LamassuSupport/lamascripts/master/patch/ticker-bignumber/kraken.js
 echo
 supervisorctl restart lamassu-server lamassu-admin-server
 echo
