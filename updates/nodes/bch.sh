578e7dd82303013958dd8ccca33c8be6f667a9b6
Lamassu Support <support@lamassu.is>
2019-07-25 12:14:59 -0400

    Update bch.sh

--- updates/nodes/bch.sh
+++ updates/nodes/bch.sh
@@ -4,14 +4,14 @@ set -e
 echo 'Updating Bitcoin Cash. This may take a minute.'
 supervisorctl stop bitcoincash
 echo 'Downloading...'
-curl -#o /tmp/bitcoincash.tar.gz https://download.bitcoinabc.org/0.19.5/linux/bitcoin-abc-0.19.5-x86_64-linux-gnu.tar.gz
+curl -#o /tmp/bitcoincash.tar.gz https://download.bitcoinabc.org/0.19.10/linux/bitcoin-abc-0.19.10-x86_64-linux-gnu.tar.gz
 tar -xzf /tmp/bitcoincash.tar.gz -C /tmp/
 echo 'Updating...'
 mv /usr/local/bin/bitcoincashd /usr/local/bin/bitcoincashd-old
 mv /usr/local/bin/bitcoincash-cli /usr/local/bin/bitcoin-cli-old
-cp /tmp/bitcoin-abc-0.19.5/bin/bitcoind /usr/local/bin/bitcoincashd
-cp /tmp/bitcoin-abc-0.19.5/bin/bitcoin-cli /usr/local/bin/bitcoincash-cli
-rm -r /tmp/bitcoin-abc-0.19.5
+cp /tmp/bitcoin-abc-0.19.10/bin/bitcoind /usr/local/bin/bitcoincashd
+cp /tmp/bitcoin-abc-0.19.10/bin/bitcoin-cli /usr/local/bin/bitcoincash-cli
+rm -r /tmp/bitcoin-abc-0.19.10
 rm /tmp/bitcoincash.tar.gz
 supervisorctl start bitcoincash
-echo 'Bitcoin Cash is updated and ready for the May 2019 protocol upgrade.'
+echo 'Bitcoin Cash is updated.'
