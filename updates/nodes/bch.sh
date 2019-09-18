54ad45adce41c422ef0adcbdec1e5473e4f0a16d
Lamassu Support <support@lamassu.is>
2019-09-18 12:39:00 -0400

    Update bch.sh

--- updates/nodes/bch.sh
+++ updates/nodes/bch.sh
@@ -4,14 +4,14 @@ set -e
 echo 'Updating Bitcoin Cash. This may take a minute.'
 supervisorctl stop bitcoincash
 echo 'Downloading...'
-curl -#o /tmp/bitcoincash.tar.gz https://download.bitcoinabc.org/0.20.0/linux/bitcoin-abc-0.20.0-x86_64-linux-gnu.tar.gz
+curl -#o /tmp/bitcoincash.tar.gz https://download.bitcoinabc.org/0.20.2/linux/bitcoin-abc-0.20.2-x86_64-linux-gnu.tar.gz
 tar -xzf /tmp/bitcoincash.tar.gz -C /tmp/
 echo 'Updating...'
 mv /usr/local/bin/bitcoincashd /usr/local/bin/bitcoincashd-old
 mv /usr/local/bin/bitcoincash-cli /usr/local/bin/bitcoin-cli-old
-cp /tmp/bitcoin-abc-0.20.0/bin/bitcoind /usr/local/bin/bitcoincashd
-cp /tmp/bitcoin-abc-0.20.0/bin/bitcoin-cli /usr/local/bin/bitcoincash-cli
-rm -r /tmp/bitcoin-abc-0.20.0
+cp /tmp/bitcoin-abc-0.20.2/bin/bitcoind /usr/local/bin/bitcoincashd
+cp /tmp/bitcoin-abc-0.20.2/bin/bitcoin-cli /usr/local/bin/bitcoincash-cli
+rm -r /tmp/bitcoin-abc-0.20.2
 rm /tmp/bitcoincash.tar.gz
 sed -i 's/\<connections\>/maxconnections/g' /mnt/blockchains/bitcoincash/bitcoincash.conf
 supervisorctl start bitcoincash
