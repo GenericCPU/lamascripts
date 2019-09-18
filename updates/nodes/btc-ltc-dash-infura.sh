086617d28b908eb6b5272f3d99cd8589e331112c
Lamassu Support <support@lamassu.is>
2019-09-18 10:31:23 -0400

    Create btc-ltc-dash-infura.sh

---
+++ updates/nodes/btc-ltc-dash-infura.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+set -e
+
+echo "Patching the tickers..."
+curl -#o $(npm root -g)/lamassu-server/lib/plugins/ticker/bitpay/bitpay.js https://raw.githubusercontent.com/LamassuSupport/lamascripts/master/patch/ticker-bignumber/bitpay.js
+curl -#o $(npm root -g)/lamassu-server/lib/plugins/ticker/bitstamp/bitstamp.js https://raw.githubusercontent.com/LamassuSupport/lamascripts/master/patch/ticker-bignumber/bitstamp.js
+curl -#o $(npm root -g)/lamassu-server/lib/plugins/ticker/kraken/kraken.js https://raw.githubusercontent.com/LamassuSupport/lamascripts/master/patch/ticker-bignumber/kraken.js
+echo
+
+# Disable geth
+echo "Disabling Geth..."
+mv /etc/supervisor/conf.d/ethereum.conf /root/ethereum.conf
+supervisorctl reread
+supervisorctl update
+echo
+
+# Update bitcoin
+echo 'Updating Bitcoin Core. This may take a minute.'
+
+curl -#o $(npm root -g)/lamassu-server/lib/plugins/wallet/bitcoind/bitcoind.js https://raw.githubusercontent.com/lamassu/lamassu-server/defiant-dingirma/lib/plugins/wallet/bitcoind/bitcoind.js
+
+supervisorctl stop bitcoin
+echo 'Downloading...'
+curl -#o /tmp/bitcoin.tar.gz https://bitcoin.org/bin/bitcoin-core-0.18.1/bitcoin-0.18.1-x86_64-linux-gnu.tar.gz
+tar -xzf /tmp/bitcoin.tar.gz -C /tmp/
+echo 'Updating...'
+mv /usr/local/bin/bitcoind /usr/local/bin/bitcoind-old
+mv /usr/local/bin/bitcoin-cli /usr/local/bin/bitcoin-cli-old
+mv /usr/local/bin/bitcoin-tx /usr/local/bin/bitcoind-tx-old
+mv /usr/local/bin/bitcoin-qt /usr/local/bin/bitcoind-qt-old
+cp /tmp/bitcoin-0.18.1/bin/* /usr/local/bin/
+rm -r /tmp/bitcoin-0.18.1
+rm /tmp/bitcoin.tar.gz
+supervisorctl start bitcoin
+echo 'Bitcoin Core is updated.'
+echo
+
+# Update dash
+echo 'Updating Dash Core. This may take a minute.'
+supervisorctl stop dash
+echo 'Downloading...'
+curl -#Lo /tmp/dash.tar.gz https://github.com/dashpay/dash/releases/download/v0.14.0.3/dashcore-0.14.0.3-x86_64-linux-gnu.tar.gz
+tar -xzf /tmp/dash.tar.gz -C /tmp/
+echo 'Updating...'
+mv /usr/local/bin/dashd /usr/local/bin/dashd-old
+mv /usr/local/bin/dash-cli /usr/local/bin/dash-cli-old
+mv /usr/local/bin/dash-tx /usr/local/bin/dash-tx-old
+mv /usr/local/bin/dash-qt /usr/local/bin/dash-qt-old
+cp /tmp/dashcore-0.14.0/bin/* /usr/local/bin/
+rm -r /tmp/dashcore-0.14.0
+rm /tmp/dash.tar.gz
+supervisorctl start dash
+echo 'Dash Core is updated.'
+echo 
+
+# Update litecoin
+echo 'Updating Litecoin Core. This may take a minute.'
+
+curl -#o $(npm root -g)/lamassu-server/lib/plugins/wallet/litecoind/litecoind.js https://raw.githubusercontent.com/lamassu/lamassu-server/defiant-dingirma/lib/plugins/wallet/litecoind/litecoind.js
+
+supervisorctl stop litecoin
+echo 'Downloading...'
+curl -#o /tmp/litecoin.tar.gz https://download.litecoin.org/litecoin-0.17.1/linux/litecoin-0.17.1-x86_64-linux-gnu.tar.gz
+tar -xzf /tmp/litecoin.tar.gz -C /tmp/
+echo 'Updating...'
+mv /usr/local/bin/litecoind /usr/local/bin/litecoind-old
+mv /usr/local/bin/litecoin-cli /usr/local/bin/litecoin-cli-old
+mv /usr/local/bin/litecoin-tx /usr/local/bin/litecoin-tx-old
+mv /usr/local/bin/litecoin-qt /usr/local/bin/litecoin-qt-old
+cp /tmp/litecoin-0.17.1/bin/* /usr/local/bin/
+rm -r /tmp/litecoin-0.17.1
+rm /tmp/litecoin.tar.gz
+supervisorctl start litecoin
+echo 'Litecoin Core is updated.'
+echo
